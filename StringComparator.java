dfe83b4aa52f20adbaeb22d8e15c4f10e3df58f9
ajbrito1993 <34195007+ajbrito1993@users.noreply.github.com>
2019-01-04 12:55:14 -0500

    Update StringComparator.java

--- StringComparator.java
+++ StringComparator.java
@@ -1,1 +1,32 @@
-S
+package com.company;
+
+import java.awt.peer.CanvasPeer;
+import java.util.Comparator;
+
+public class StringComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+
+        int length1 = o1.length();
+        int length2 = o2.length();
+
+        if (length1 < length2){
+            System.out.println("The first name's length is smaller than the second name's length.");
+            return 1;
+        }
+
+        else if (length2 > length1) {
+            System.out.println("The second name's length is larger than the first name's length.");
+            return 0;
+        }
+
+        else  if (length1 == length2) {
+            System.out.println("The lengths match size");
+            return 2;
+        }
+
+        return -1;
+
+    }
+}
+
